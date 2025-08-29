9f5f6318cf508a1d2fafc33d20b84a6e2dadc821
Eddhan Tan <eddhan12345@gmail.com>
2025-08-29 16:02:38 +0800

    added add task buttons

--- script.js
+++ script.js
@@ -1,55 +1,85 @@
-const board = document.getElementById('board')
+const board = document.getElementById("board");
 
 // styles
-const columnStyle = 'column'
-const cardStyle = 'card'
-const columnTitleStyle = 'font-semibold text-gray-800 text-xl mb-4'
-const cardGridStyle = 'grid grid-cols-1 gap-4'
+const columnStyle = "column";
+const cardStyle = "card";
+const columnNameStyle = "font-semibold text-gray-800 text-xl";
+const cardGridStyle = "grid grid-cols-1 gap-4";
+const addButtonStyle =
+  "add bg-sky-600 text-white font-semibold px-4 py-2 rounded hover:bg-sky-800 cursor-pointer";
 
 const columns = [
   { id: 1, name: "backlogs" },
   { id: 2, name: "in-progress" },
-  { id: 3, name: "done" }
+  { id: 3, name: "done" },
 ];
 
 const tasks = [
-  {id: 1, name: "Practice javascript", columnId: 2},
-  {id: 2, name: "Clean the room", columnId: 1},
-  {id: 3, name: "Do homeworks", columnId: 1},
-  {id: 4, name: "Take a bath", columnId: 3},
-  {id: 5, name: "Go to school", columnId: 3},
-  {id: 6, name: "Push code to GitHub", columnId: 1}
-]
+  { id: 1, name: "Practice javascript", columnId: 2 },
+  { id: 2, name: "Clean the room", columnId: 1 },
+  { id: 3, name: "Do homeworks", columnId: 1 },
+  { id: 4, name: "Take a bath", columnId: 3 },
+  { id: 5, name: "Go to school", columnId: 3 },
+  { id: 6, name: "Push code to GitHub", columnId: 1 },
+  { id: 7, name: "Listen to spotify", columnId: 2 },
+];
 
-//render each column
-columns.forEach(column => {
-  //for each column make a div
-  const columnDiv = document.createElement('div')
-  columnDiv.className = columnStyle
+function renderColumns() {
+  board.innerHTML = "";
 
-  //add the title of the column
-  const columnTitle = document.createElement('p')
-  columnTitle.className = columnTitleStyle
-  columnTitle.textContent = column.name
+  columns.forEach((column) => {
+    const columnDiv = document.createElement("div");
+    columnDiv.className = columnStyle;
 
-  //make the container of the cards
-  const cardsContainer = document.createElement('div')
-  cardsContainer.className = cardGridStyle
+    const columnName = document.createElement("p");
+    columnName.className = columnNameStyle;
+    columnName.textContent = column.name;
 
-  columnDiv.appendChild(columnTitle)
-  columnDiv.appendChild(cardsContainer)
+    columnDiv.appendChild(columnName);
 
-  //get all the tasks in this column
-  const colTasks = tasks.filter(task => task.columnId === column.id)
+    const cardGrid = document.createElement("div");
+    cardGrid.className = cardGridStyle;
 
-  //for each tasks make a card and put it in card container
-  colTasks.forEach(task => {
-    const taskCard = document.createElement('div')
-    taskCard.className = cardStyle
-    taskCard.textContent = task.name
-    cardsContainer.appendChild(taskCard)
-  })
+    columnDiv.appendChild(cardGrid);
 
-  //add the columns to the board
-  board.appendChild(columnDiv)
-})
+    const columnTasks = tasks.filter((task) => task.columnId === column.id);
+    columnTasks.forEach((task) => {
+      const taskCard = document.createElement("div");
+      taskCard.className = cardStyle;
+      taskCard.textContent = task.name;
+      cardGrid.appendChild(taskCard);
+    });
+
+    const btnAddTask = document.createElement("button");
+    btnAddTask.textContent = "Add task";
+    btnAddTask.dataset.colId = column.id;
+    btnAddTask.className = addButtonStyle;
+
+    columnDiv.appendChild(btnAddTask);
+
+    board.appendChild(columnDiv);
+  });
+}
+
+renderColumns();
+
+board.addEventListener("click", (e) => {
+  const addButton = e.target.closest(".add");
+  if (addButton) {
+    const columnId = Number(addButton.dataset.colId);
+    const newTask = window.prompt("Enter new task");
+    if (!newTask) return;
+    addNewTask(newTask, columnId);
+  }
+});
+
+function addNewTask(newTask, columnId) {
+  let id = 0;
+  tasks.forEach((task) => {
+    const storedId = task.id;
+    if (id < storedId) id = storedId;
+  });
+  id++;
+  tasks.push({ id: id, name: newTask, columnId: columnId });
+  renderColumns();
+}
